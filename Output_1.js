1da29e82a37c67620efd86fc914e7e5e0b04d362
Rofiqul Alam Shehab <58635957+shehab0911@users.noreply.github.com>
2023-08-30 20:49:08 +0600

    Add files via upload

---
+++ Output_1.js
@@ -0,0 +1,10 @@
+console.log("bangladesh")
+var x = 3, y = 4, z;
+z = x + y
+document.write("output of the variable : ", z)
+
+subject = ["english", "math", "bangla"]
+
+
+var x = prompt("please enter your name: ")
+document.write("number of character : ", x.length, "index number :", x.)
